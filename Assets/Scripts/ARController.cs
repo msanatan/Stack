6f31634f57489f95f9af6c85e05cbf772881ce22
Marcus Sanatan <msanatan@gmail.com>
2022-10-02 12:51:34 -0400

    Add script to create object on plane when the screen is touched
    It's a simple script to start us off

---
+++ Assets/Scripts/ARController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARController : MonoBehaviour
+{
+    [SerializeField] GameObject cube;
+    [SerializeField] ARRaycastManager raycastManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var touchInput = Input.GetTouch(0);
+        if (Input.touchCount > 0 && touchInput.phase == TouchPhase.Began)
+        {
+            var touches = new List<ARRaycastHit>();
+            raycastManager.Raycast(touchInput.position, touches, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+            if (touches.Count > 0)
+            {
+                GameObject.Instantiate(cube, touches[0].pose.position, touches[0].pose.rotation);
+            }
+        }
+    }
+}
