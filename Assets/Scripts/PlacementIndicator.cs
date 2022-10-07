da101db345db2792b2d484d5e2e2675c7a21b2ed
Marcus Sanatan <msanatan@gmail.com>
2022-10-07 07:19:54 -0400

    Add placement indicator logic
    It raycasts from the centre of the screen to a plane
    That's what I expected, glad to see how it works in AR.

--- Assets/Scripts/PlacementIndicator.cs
+++ Assets/Scripts/PlacementIndicator.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class PlacementIndicator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    ARRaycastManager raycastManager;
+    GameObject plane;
+    Vector2 screenDimensions;
+
     void Start()
     {
-        
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        plane = transform.GetChild(0).gameObject; // Gets the plane object
+        plane.SetActive(false); // Off by default
+        screenDimensions = new Vector2(Screen.width / 2, Screen.height / 2);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Shoot raycast from centre of screen
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenDimensions, hits, TrackableType.Planes);
+
+        // If we hit AR plan, update position and rotation of marker
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!plane.activeInHierarchy)
+            {
+                plane.SetActive(true);
+            }
+        }
     }
 }
